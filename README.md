9bd083857a624480e8c4e0009fc149e0366c5d42
Ed Belleza <119173327+edmacb@users.noreply.github.com>
2023-07-20 22:44:38 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Construction-Ed-Code-MNL
 
 Final Project on Code MNL 1 Month Bootcamp
+
 updated July 20, 2023 on Section 4
+updated July 20, 2023 on Hero
--- scripts.js
+++ scripts.js
@@ -38,3 +38,107 @@ class CS_GalleryFilter {
 }
 
 new CS_GalleryFilter();
+
+// JavaScript logic for carousel/slideshow
+const slides = document.querySelectorAll(".carousel-slide");
+let currentSlide = 0;
+let isDragging = false;
+let startPos = 0;
+let currentTranslate = 0;
+let prevTranslate = 0;
+let interval = null; // Variable to store the autoplay interval
+
+function showSlide(slideIndex) {
+  slides.forEach((slide, index) => {
+    slide.classList.remove("active");
+    if (index === slideIndex) {
+      slide.classList.add("active");
+      slide.querySelectorAll(".slide-animation").forEach((element) => {
+        element.classList.remove("slide-animation");
+        void element.offsetWidth; // Trigger reflow to restart animation
+        element.classList.add("slide-animation");
+      });
+    }
+  });
+}
+
+function previousSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function getPositionX(event) {
+  return event.type.includes("mouse") ? event.pageX : event.touches[0].clientX;
+}
+
+function touchStart(event) {
+  isDragging = true;
+  startPos = getPositionX(event);
+}
+
+function touchMove(event) {
+  if (isDragging) {
+    const currentPosition = getPositionX(event);
+    currentTranslate = prevTranslate + currentPosition - startPos;
+  }
+}
+
+function touchEnd() {
+  isDragging = false;
+  const moveBy = currentTranslate - prevTranslate;
+
+  if (moveBy < -100) {
+    nextSlide();
+  } else if (moveBy > 100) {
+    previousSlide();
+  }
+
+  currentTranslate = 0;
+  prevTranslate = 0;
+}
+
+// Add event listeners for touch events
+slides.forEach((slide) => {
+  slide.addEventListener("touchstart", touchStart);
+  slide.addEventListener("touchmove", touchMove);
+  slide.addEventListener("touchend", touchEnd);
+});
+
+// Add event listeners for mouse events
+slides.forEach((slide) => {
+  slide.addEventListener("mousedown", touchStart);
+  slide.addEventListener("mousemove", touchMove);
+  slide.addEventListener("mouseup", touchEnd);
+  slide.addEventListener("mouseleave", touchEnd);
+});
+
+// Autoplay function
+function startAutoplay() {
+  interval = setInterval(() => {
+    nextSlide();
+  }, 10000); // Adjust the autoplay duration as needed (in milliseconds)
+}
+
+function stopAutoplay() {
+  clearInterval(interval);
+}
+
+// Start autoplay when the page loads
+startAutoplay();
+
+// Pause autoplay on carousel interaction
+slides.forEach((slide) => {
+  slide.addEventListener("mousedown", stopAutoplay);
+  slide.addEventListener("touchstart", stopAutoplay);
+});
+
+// Resume autoplay after carousel interaction
+slides.forEach((slide) => {
+  slide.addEventListener("mouseup", startAutoplay);
+  slide.addEventListener("touchend", startAutoplay);
+});
